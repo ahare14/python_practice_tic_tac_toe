4332d1c787ca87191ba3d1b0e55e571bd5f18752
ahare14 <alexanderhare14@gmail.com>
2019-10-04 13:03:40 -0600

    updated readme

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,3 +13,12 @@ def display_board(board):
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
+
+def player_input():
+  marker = ""
+  while not (marker == 'X' or marker == 'O'):
+    marker = input('Player 1: Do you want to be X or O? ').upper()
+  if marker == "X":
+    return ("X", "O")
+  else:
+    return ("O", "X")
